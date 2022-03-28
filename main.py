30205bfd3207fb53c7d04c5ee77fc0cbd921aeab
TH1RT3EN-LI <litianshun20001220@gmail.com>
2022-03-28 11:48:57 +0800

    V-0,1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turn_to_character import TurnToCharacter
+
 """        ,----,       ,----,           
       ,/   .`|     ,/   .`|           
     ,`   .'  :   ,`   .'  : ,----..   
@@ -17,22 +18,16 @@ from turn_to_character import TurnToCharacter
                                       """
 if __name__ == '__main__':
     # 供替换的字符，长度应小于等于灰度位数
-    character_list = [
-        '一',
-        '二',
-        '三',
-        '四',
-        '五',
-        '六',
-        '七',
-        '八',
-    ]
-    character_list.reverse()
+    character_list = list('一二三四五六七八')
+ 
     # 图片路径
-    path = r''
+    path = r'Turn To Character\test.jpeg'
+
     # 缩放倍数
     multiple = 4
+
     # 实例化
     tc = TurnToCharacter(path, multiple)
+
     # 选择是否需要放大、缩小
     tc.turn_to_character(character_list, zoom_in=False, zoom_out=True)
